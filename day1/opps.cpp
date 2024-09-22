03a887019f9f5b41d7a86e6933d5b3b3929918b1
MuskanKumari23 <muskankumari92084@Gmail.com>
2024-09-22 21:17:13 +0530

    Initial commit

---
+++ day1/opps.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+ public:
+ Class Teacher {
+    string name;
+    string dep;
+    string subject;
+
+ };
+ int main(){
+    Teacher t1;
+    t1.name= "xyz";
+    cout<<t1.name<<endl;
+ }
+ {
+    return 0;
+ }
+
+     
+
+
+
