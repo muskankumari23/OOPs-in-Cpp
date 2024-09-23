ef3b44695b06913b91f5f759f98d435703c7d9d0
MuskanKumari23 <muskankumari92084@Gmail.com>
2024-09-23 22:13:55 +0530

    create constructor.cpp

---
+++ day1/constructor.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include <string>
+ using namespace std;
+ class Teacher {
+   
+    public:
+    Teacher (){//non paarameterize 
+    dept="CS";
+    }
+    string name;
+    string dept;
+    }
+    int main (){
+        Teacher t1 ("abc"," cde",);
+        return 0;
+
+    }
+
+
