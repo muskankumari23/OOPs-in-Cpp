3eb0ad3e6bde25b578dd043c6d69455088fb8035
MuskanKumari23 <muskankumari92084@Gmail.com>
2024-09-24 21:05:19 +0530

    .

--- DAY2/ptr.cpp
+++ DAY2/ptr.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <string>
  using namespace std;
-  class Teacher (string name, string dept, string subject );
+  class Teacher (String name, String dept, String subject );{
+
+
   this->name =name; 
   this->dept =dept;
   this->subject=subject;
-  void getInfo(){
+  void getInfo() }
+  {
     cout<< "name :"<< name <<endl;
   }
   int main(){
     Teacher t1 ("abc ", "def ","ghi");
     t1.getInfo();
     return 0;
-
   }
   
